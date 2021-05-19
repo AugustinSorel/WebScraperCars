636cb9bc8b79dcaa3e8bbb6f2699afa53d3287ba
tartie sorel <sorelaugustin@gmail.com>
2021-05-19 17:58:38 +0100

    gridview added

---
+++ WebScraperCars/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace WebScraperCars
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        #region Property Changed Event Handler 
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
--- WebScraperCars/ViewModels/MainPageViewModel.cs
+++ WebScraperCars/ViewModels/MainPageViewModel.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebScraperCars.ViewModels
+﻿namespace WebScraperCars.ViewModels
 {
-    class MainPageViewModel
+    class MainPageViewModel : ViewModelBase
     {
+        
     }
 }
