ac205f5e251913f877eee485c3cf89e6307fd4e5
tartie sorel <sorelaugustin@gmail.com>
2021-05-19 19:11:19 +0100

    dummy data removed

--- WebScraperCars/ViewModels/MainPageViewModel.cs
+++ WebScraperCars/ViewModels/MainPageViewModel.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using WebScraperCars.Models;
 
 namespace WebScraperCars.ViewModels
 {
     class MainPageViewModel : ViewModelBase
     {
-        public ObservableCollection<CarModel> carModels { get; set; }
-        public List<Books> Books { get; set; }
-
+        public ObservableCollection<CarModel> CarModels { get; set; }
+        
         public MainPageViewModel()
         {
-            Books = BookManager.GetBooks();
-            carModels = CarManager.GetCars();
+            CarModels = CarManager.GetCars();
         }
     }
 }
