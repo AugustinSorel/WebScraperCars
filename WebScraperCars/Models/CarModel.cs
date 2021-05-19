a9303c8da394fa56d6dac71d8dfa42a7c738051f
tartie sorel <sorelaugustin@gmail.com>
2021-05-19 18:45:24 +0100

    car model added

---
+++ WebScraperCars/Models/CarModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebScraperCars.Models
+{
+    class CarModel
+    {
+        public int CarID { get; set; }
+        public float CarPrice { get; set; }
+        public string CarName { get; set; }
+        public string CarCountry { get; set; }
+        public string CarImage { get; set; }
+    }
+}
