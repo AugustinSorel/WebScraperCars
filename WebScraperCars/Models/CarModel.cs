118423e6918ba4b3f3c082bb48508547c7ca3ddf
tartie sorel <sorelaugustin@gmail.com>
2021-05-20 12:13:20 +0100

    get le parking cars

--- WebScraperCars/Models/CarModel.cs
+++ WebScraperCars/Models/CarModel.cs
@@ -1,33 +1,13 @@
-﻿using System.Collections.ObjectModel;
-
-namespace WebScraperCars.Models
+﻿namespace WebScraperCars.Models
 {
     class CarModel
     {
-        public int CarID { get; set; }
-        public float CarPrice { get; set; }
+        public string CarID { get; set; }
+        public string CarPrice { get; set; }
         public string CarName { get; set; }
         public string CarCountry { get; set; }
         public string CarImage { get; set; }
         public string CarSite { get; set; }
         public string URL { get; set; }
-    }
-
-    class CarManager
-    {
-        public static ObservableCollection<CarModel> GetCars()
-        {
-            var cars = new ObservableCollection<CarModel>()
-            {
-                new CarModel(){CarID = 1212493493, CarCountry = "France", CarName = "Audi", CarPrice = 1000, CarImage = "Assets/audi-r8-.jpg", CarSite="Le Parking" },
-                new CarModel(){CarID = 11323, CarCountry = "France", CarName = "BMW", CarPrice = 17200, CarImage = "Assets/BMW.jpg", CarSite="La Central" },
-                new CarModel(){CarID = 12239289, CarCountry = "Angleterre", CarName = "Audi", CarPrice = 1023, CarImage = "Assets/audi-r8-.jpg", CarSite="La Central" },
-                new CarModel(){CarID = 1829842, CarCountry = "Allemagne", CarName = "Renault", CarPrice = 7381, CarImage = "Assets/audi-r8-.jpg", CarSite="Le Bon Coin" },
-                new CarModel(){CarID = 193613, CarCountry = "Portugale", CarName = "BMW", CarPrice = 190293, CarImage = "Assets/BMW.jpg", CarSite="Le Parking" },
-                new CarModel(){CarID = 12111, CarCountry = "France", CarName = "Audi", CarPrice = 293, CarImage = "Assets/Audi2.jpg", CarSite="Le Bon Coin" },
-            };
-
-            return cars;
-        }
     }
 }
--- WebScraperCars/ViewModels/MainPageViewModel.cs
+++ WebScraperCars/ViewModels/MainPageViewModel.cs
@@ -1,5 +1,11 @@
-﻿using System.Collections.ObjectModel;
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using WebScraperCars.Models;
 
 namespace WebScraperCars.ViewModels
@@ -24,9 +30,71 @@ namespace WebScraperCars.ViewModels
             ButtonScrapingCommand = new StartScrapingCommand(StartScraping);
         }
 
-        public void StartScraping(object carName)
+        public async void StartScraping(object carName)
         {
-            CarModelsItemSource = CarManager.GetCars();
+            CarModelsItemSource = await GetCars(carName as string);
+        }
+
+        private async Task<ObservableCollection<CarModel>> GetCars(string v)
+        {
+            var url = "https://www.leparking.fr/voiture-occasion/" + v + ".html";
+            ObservableCollection<CarModel> carModels = new ObservableCollection<CarModel>();
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+
+            htmlDocument.LoadHtml(await new HttpClient().GetStringAsync(url));
+
+            var productHTML = htmlDocument.DocumentNode.Descendants("ul").Where(node => node.GetAttributeValue("class", "").Equals("resultat")).ToList();
+
+            foreach (HtmlNode productListItem in GetProductLists(productHTML))
+            {
+                carModels.Add(new CarModel()
+                {
+                    CarID = GetItemID(productListItem),
+                    CarCountry = GetCountry(productListItem),
+                    CarName = GetTitle(productListItem),
+                    CarPrice = GetPrice(productListItem),
+                    CarSite = "Le Parking",
+                    URL = "NO",
+                    CarImage = GetImage(productListItem),
+                });
+            }
+
+            return carModels;
+        }
+
+        private string GetImage(HtmlNode productListItem)
+        {
+            string imageString = productListItem.Descendants("img").FirstOrDefault().GetAttributeValue("src", "").Trim();
+
+            if (imageString.Length < 40)
+                return "Assets/ImageNotAvailable.png";
+
+            return imageString;
+        }
+
+        private string GetItemID(HtmlNode productListItem)
+        {
+            return productListItem.GetAttributeValue("tref", "").Trim();
+        }
+
+        private string GetTitle(HtmlNode productListItem)
+        {
+            return productListItem.Descendants("span").Where(node => node.GetAttributeValue("class", "").Equals("title-block brand")).FirstOrDefault().InnerText.Trim();
+        }
+
+        private string GetCountry(HtmlNode productListItem)
+        {
+            return productListItem.Descendants("span").Where(node => node.GetAttributeValue("Class", "").Equals("upper")).FirstOrDefault().InnerText.Trim();
+        }
+
+        private string GetPrice(HtmlNode productListItem)
+        {
+            return productListItem.Descendants("p").Where(node => node.GetAttributeValue("Class", "").Equals("prix")).FirstOrDefault().InnerText.Trim();
+        }
+
+        private IEnumerable<HtmlNode> GetProductLists(List<HtmlNode> productHTML)
+        {
+            return productHTML[0].Descendants("li").Where(node => node.GetAttributeValue("class", "").Contains("clearfix")).ToList();
         }
     }
 }
