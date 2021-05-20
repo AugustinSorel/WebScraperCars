b5a9b335cc20bd0136f977dd76a6d5b1b539722d
tartie sorel <sorelaugustin@gmail.com>
2021-05-20 11:19:37 +0100

    StartScrapingCommand class added

--- WebScraperCars/ViewModels/MainPageViewModel.cs
+++ WebScraperCars/ViewModels/MainPageViewModel.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Windows.Input;
 using WebScraperCars.Models;
 
 namespace WebScraperCars.ViewModels
@@ -28,32 +26,7 @@ namespace WebScraperCars.ViewModels
 
         public void StartScraping(object carName)
         {
-            Debug.WriteLine(carName as string);
             CarModelsItemSource = CarManager.GetCars();
-        }
-    }
-
-    class StartScrapingCommand : ICommand
-    {
-        public event EventHandler CanExecuteChanged;
-        private Action<object> _execute;
-
-        public StartScrapingCommand(Action<object> execute)
-        {
-            _execute = execute;
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            if (string.IsNullOrEmpty(parameter as string))
-                return false;
-
-            return true;
-        }
-
-        public void Execute(object parameter)
-        {
-            _execute.Invoke(parameter);
         }
     }
 }
---
+++ WebScraperCars/ViewModels/StartScrapingCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WebScraperCars.ViewModels
+{
+    class StartScrapingCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action<object> _execute;
+
+        public StartScrapingCommand(Action<object> execute)
+        {
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (string.IsNullOrEmpty(parameter as string))
+                return false;
+
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
+        }
+    }
+}
