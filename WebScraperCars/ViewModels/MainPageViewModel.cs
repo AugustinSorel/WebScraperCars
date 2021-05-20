fca9e62ddd9eb565a4bf5356f7135b4ced68616e
tartie sorel <sorelaugustin@gmail.com>
2021-05-20 11:09:42 +0100

    StartScrapingCommand added

--- WebScraperCars/ViewModels/MainPageViewModel.cs
+++ WebScraperCars/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Input;
 using WebScraperCars.Models;
 
 namespace WebScraperCars.ViewModels
@@ -6,7 +9,8 @@ namespace WebScraperCars.ViewModels
     class MainPageViewModel : ViewModelBase
     {
         private ObservableCollection<CarModel> carModelsItemSource;
-        
+        public StartScrapingCommand ButtonScrapingCommand { get; set; }
+
         public ObservableCollection<CarModel> CarModelsItemSource
         {
             get { return carModelsItemSource; }
@@ -19,7 +23,37 @@ namespace WebScraperCars.ViewModels
 
         public MainPageViewModel()
         {
+            ButtonScrapingCommand = new StartScrapingCommand(StartScraping);
+        }
+
+        public void StartScraping(object carName)
+        {
+            Debug.WriteLine(carName as string);
             CarModelsItemSource = CarManager.GetCars();
+        }
+    }
+
+    class StartScrapingCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action<object> _execute;
+
+        public StartScrapingCommand(Action<object> execute)
+        {
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (string.IsNullOrEmpty(parameter as string))
+                return false;
+
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
         }
     }
 }
